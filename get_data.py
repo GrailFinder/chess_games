2c1d8c156295e4ecdbded22cb91b059c8bfae65b
grail <wohilas@gmail.com>
2018-02-01 11:49:07 +0300

    trying to find timecorr

--- get_data.py
+++ get_data.py
@@ -12,9 +12,7 @@ def make_dfs(filename, playername):
     dat = pgn.loads(pgn_text) # Returns a list of PGNGame
     sdat = pgn.dumps(pgn_game) # Returns a string with a pgn game
 
-
     f_g = dat[0]
-
     games = list()
 
     for game in dat:
@@ -25,11 +23,7 @@ def make_dfs(filename, playername):
         games.append(game_dict)
 
     df = pd.DataFrame(games)
-
     split_timecontrol(df)
-
-    print(df.head)
-
 
     df.whiteelo = pd.to_numeric(df.whiteelo, errors='coerce')
     df.blackelo = pd.to_numeric(df.blackelo, errors='coerce')
@@ -42,7 +36,10 @@ def make_dfs(filename, playername):
     return me_black, me_white, df
 
 def split_timecontrol(df):
+    # http://pandas.pydata.org/pandas-docs/stable/generated/pandas.Series.str.split.html
     df[['timecontrol_base', 'increment']] = df.timecontrol.str.split('+', expand=True)
+    df.timecontrol_base = pd.to_numeric(df.timecontrol_base, errors='coerce')
+    df.increment = pd.to_numeric(df.increment, errors='coerse')
     return df
 
 
@@ -67,20 +64,18 @@ def show_openings(df, color='black'):
     plt.tight_layout()
     plt.show()
 
-def show_corr(df):
-    #df.timecontrol = pd.to_numeric(df.timecontrol, errors='coerce')
-    print(df.timecontrol)
-    sns.swarmplot(x="result", y="opening", data=df)
+def show_timecorr(df):
+    sns.swarmplot(x="result", y="timecontrol_base", hue='increment', data=df)
     plt.show()
 
 gd_black, gd_white, gd_both = make_dfs(filename='lichess_grossmendPro_2018-01-30.pgn', playername='grossmendPro')
 me_black, me_white, me_both = make_dfs(filename='lichess_GrailFinder_2018-01-30.pgn', playername='GrailFinder')
 
-show_openings(gd_black)
-show_openings(gd_white)
+# show_openings(gd_black)
+# show_openings(gd_white)
 
-show_openings(me_black)
-show_openings(me_white)
+# show_openings(me_black)
+# show_openings(me_white)
 
-# show_corr(me_both)
-# show_corr(gd_both)
+show_timecorr(me_black)
+show_timecorr(me_white)
